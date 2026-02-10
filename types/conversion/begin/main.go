7278278135e6715e38ffc2f08a1c0de672c3da2e
JavaGeek <buntycool54@gmail.com>
2026-02-10 11:53:48 +0000

    commit types

--- types/conversion/begin/main.go
+++ types/conversion/begin/main.go
@@ -1,6 +1,14 @@
 // types/conversion/begin/main.go
 package main
 
+import "fmt"
+
 func main() {
 	// declare float and convert it to an unsigned int
+ a:= 42.5
+ b:= uint(a)
+ fmt.Printf("a: %v - %T\n", a, a)
+ fmt.Printf("b: %v - %T\n", b, b)
+
+
 }
